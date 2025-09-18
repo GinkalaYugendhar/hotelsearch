758ddc32cd0fd779574056d5684b590c4368b867
yugendhar <ginkalayugendhar@gmail.com>
2025-09-18 10:18:48 +0530

    api spec

---
+++ src/test/java/com/hg/hotelsearch/HotelsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hg.hotelsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
